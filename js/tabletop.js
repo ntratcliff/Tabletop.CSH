acc288bad06c33df44cf382d99dbbecb0e11821c
Steven Mirabito <steven@stevenmirabito.com>
2015-10-20 01:09:39 -0400

    Auto-dismiss modal after submitting registration form and collapse the responsive navbar after clicking a navigation item on mobile.

--- js/tabletop.js
+++ js/tabletop.js
@@ -18,6 +18,9 @@ $(document).ready(function () {
         // Show the requested page
         $('#page-' + pageToNavigate).show().addClass('page-active');
 
+        // Collapse the navbar
+        $("#navbar").collapse('hide');
+
         // Prevent the browser from navigating anywhere
         e.preventDefault();
         return false;
@@ -70,6 +73,10 @@ $(document).ready(function () {
                     'entry.1939605299': $("input#phone").val()
                 },
                 success: function (data) {
+                    // Hide the form and modal footer
+                    $("#magicFormContainer").hide();
+                    $("#magicFormFooter").hide();
+
                     // Clear the inputs
                     $("input#name").val('');
                     $("input#email").val('');
@@ -78,15 +85,20 @@ $(document).ready(function () {
                     // Show success alert
                     $("#magicFormAlert").addClass("alert-success").html("<strong>Thanks!</strong> Your registration has been received. See you there!").show();
 
-                    // Close the alert in 5 seconds
-                    setTimeout(function(){ closeAlert("#magicFormAlert"); }, 5000);
+                    // Dismiss the alert and modal after 5 seconds
+                    setTimeout(function(){
+                        closeAlert("#magicFormAlert");
+                        $("#magicModal").modal('hide');
+                    }, 5000);
                 },
                 error: function (jqXHR, textStatus, errorThrown) {
                     // Show failure alert
                     $("#magicFormAlert").addClass("alert-danger").html("<strong>Whoops!</strong> Something went wrong. Please try submitting again in a few minutes.").show();
 
-                    // Close the alert in 5 seconds
-                    setTimeout(function(){ closeAlert("#magicFormAlert"); }, 5000);
+                    // Dismiss the alert after 5 seconds
+                    setTimeout(function(){
+                        closeAlert("#magicFormAlert");
+                    }, 5000);
 
                     // Log error to console
                     console.log("An error occurred while trying to submit the form: " + textStatus, errorThrown);
